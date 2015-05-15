641df4fe194de43b59bb2b7699cd6faa44f8891a
Hugh Sutton-Gee <hsuttong@gmail.com>
2015-05-14 21:57:35 -0700

    add maps and some busted js

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,6 +9,24 @@ post '/breweries' do
   #do shit here to post to brewererydb api
   api = Brewerydb::Client.new
   @breweries = api.brew_by_city(params[:city])
+  @city_brew = []
+  @breweries["data"].each do |brew|
+    lat = brew["latitude"]
+    long = brew["latitude"]
+    name = brew["brewery"]["name"]
+    @city_brew << [name, lat, long]
+
+    puts "#{lat}, #{long}, #{name}"
+
+  end
+  binding.pry
+  p @city_brew
+
   erb :index
+end
+
+get '/maps' do
+  GOOG_KEY = ENV['GOOGLE_API_KEY']
+  erb :map
 end
 
---
+++ notes.md
@@ -0,0 +1,20 @@
+#### dev notes
+
+
+
+- Make a form to submit city to get lat/long via httparty
+    -play with brewerydb api with postman and see how to develop query
+- Get the JSON from brewerydb
+- Save the json somewhere
+- start plotting.
+
+
+brewerydb
+ed58bc32118b9b8dc8aa93da988936a0
+
+
+
+ideas:
+-you could a brewery growth heat map visualiztion, like:
+https://embed-dot-more-than-a-map.appspot.com/demos/visualization/population
+
