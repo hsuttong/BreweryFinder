04f2f4e1fbd30b20ea61fe823c8df477ff2412df
Hugh Sutton-Gee <hsuttong@gmail.com>
2016-03-04 19:57:51 -0800

    require pp

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 require 'awesome_print'
+require 'pp'
 
 if %w{development test}.include?(ENV['RACK_ENV'])
   require 'dotenv'
